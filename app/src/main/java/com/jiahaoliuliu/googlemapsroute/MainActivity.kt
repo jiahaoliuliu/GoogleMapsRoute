6368a7a6b7a3dea9b01d13ff00551f528ae13b72
Jiahao Liu <jiahao.liu@dnata.com>
2020-04-13 11:29:32 +0400

    Created a simple google maps integration

--- app/src/main/java/com/jiahaoliuliu/googlemapsroute/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/googlemapsroute/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.jiahaoliuliu.googlemapsroute
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Marker
+import com.jiahaoliuliu.googlemapsroute.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GoogleMap.OnMarkerClickListener {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
+        val locationFragment = LocationFragment()
+        locationFragment.setOnMarkerClickListener(this)
+        supportFragmentManager.beginTransaction().add(R.id.container, locationFragment).commit()
+    }
+
+    override fun onMarkerClick(marker: Marker?): Boolean {
+//        val damagesFragment = DamagesFragment()
+//        supportFragmentManager
+//            .beginTransaction()
+//            .replace(R.id.container, damagesFragment)
+//            .addToBackStack("DamagesFragment")
+//            .commit()
+        return true
     }
 }
