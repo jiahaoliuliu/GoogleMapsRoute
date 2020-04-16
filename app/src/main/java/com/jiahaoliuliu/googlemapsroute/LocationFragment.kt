8d6798a6621f29cfd7361f2c486d6eb696e737f7
Jiahao Liu <jiahao.liu@dnata.com>
2020-04-16 13:40:13 +0400

    Drawing the marker in the middle of the route

--- app/src/main/java/com/jiahaoliuliu/googlemapsroute/LocationFragment.kt
+++ app/src/main/java/com/jiahaoliuliu/googlemapsroute/LocationFragment.kt
@@ -1,11 +1,14 @@
 package com.jiahaoliuliu.googlemapsroute
 
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -13,10 +16,7 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.model.*
 import com.google.maps.android.PolyUtil
 import com.jiahaoliuliu.datalayer.DirectionRepository
 import com.jiahaoliuliu.datalayer.DistanceRepository
@@ -25,13 +25,16 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
 import javax.inject.Inject
+import kotlin.math.acos
+import kotlin.math.cos
+import kotlin.math.sin
 
 class LocationFragment: Fragment() {
 
     companion object {
         private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1000
         private val DEFAULT_LOCATION = LatLng(25.276, 55.296)
-        private val DXB_AIRPORT_LOCATION = LatLng(25.2527777777778, 55.3644444444444)
+        private val DXB_AIRPORT_LOCATION = LatLng(25.253176, 55.365673)
         private const val DEFAULT_ZOOM = 15F
     }
 
@@ -41,6 +44,7 @@ class LocationFragment: Fragment() {
     private var locationPermissionGranted = false
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private var lastKnownLocation: Location? = null
+    private var userPositionMarker: Marker? = null
     private var onMarkerClickListener: GoogleMap.OnMarkerClickListener? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -159,12 +163,23 @@ class LocationFragment: Fragment() {
             Coordinate(DXB_AIRPORT_LOCATION.latitude, DXB_AIRPORT_LOCATION.longitude))
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                        direction -> Timber.v("direction Returned $direction")
+                .subscribe({direction ->
+                    val positions = PolyUtil.decode(direction.polyline)
+                    // Add the lines
                     googleMap?.addPolyline(
                         PolylineOptions()
                             .color(ContextCompat.getColor(context!!, R.color.colorRoute))
-                        .addAll(PolyUtil.decode(direction.polyline)))
+                        .addAll(positions))
+                    // Add the marker
+                    val view = activity?.layoutInflater?.inflate(R.layout.direction_marker, null, false) as TextView
+                    view.text = "${direction.duration}(${direction.distance})"
+                    val bmp = loadBitmapFromView(view)
+                    val midPoint = getMidPoint(positions)
+                    googleMap?.addMarker(MarkerOptions()
+                        .position(midPoint)
+                        .anchor(0.5f, 0.5f)
+                        .icon(BitmapDescriptorFactory.fromBitmap(bmp))
+                    )
                 },
                     {throwable -> Timber.e(throwable, "Error getting the direction")}
                 )
@@ -178,7 +193,7 @@ class LocationFragment: Fragment() {
 
         val markerOptions = MarkerOptions()
         markerOptions.position(lastKnownLocationLatLng)
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
         googleMap.addMarker(markerOptions)
         googleMap.setOnMarkerClickListener { marker ->
             onMarkerClickListener?.onMarkerClick(marker)!!
@@ -187,5 +202,51 @@ class LocationFragment: Fragment() {
 
     fun setOnMarkerClickListener(onMarkerClickListener: GoogleMap.OnMarkerClickListener) {
         this.onMarkerClickListener = onMarkerClickListener
+    }
+
+    private fun loadBitmapFromView(v: View): Bitmap {
+        v.measure(
+            0,//View.MeasureSpec.makeMeasureSpec((resources.displayMetrics.density*30).toInt(), View.MeasureSpec.EXACTLY),
+            0//View.MeasureSpec.makeMeasureSpec((resources.displayMetrics.density*30).toInt(), View.MeasureSpec.EXACTLY)
+        )
+        val b = Bitmap.createBitmap(v.measuredWidth, v.measuredHeight, Bitmap.Config.ARGB_8888)
+        val  c = Canvas(b)
+        v.layout(0, 0, v.measuredWidth, v.measuredHeight)
+        v.draw(c)
+        return b
+    }
+
+    private fun getMidPoint(positions: List<LatLng>): LatLng {
+        var totalDistance = 0F
+        for(i in 0 until positions.size-1) {
+            totalDistance += distanceBetweenPoints(positions[i], positions[i+1])
+        }
+
+        val halfDistance = totalDistance/2
+        var distance = 0F
+        for(i in 0 until positions.size-1) {
+            distance += distanceBetweenPoints(positions[i], positions[i+1])
+            if(distance>halfDistance) {
+                return LatLng( (positions[i].latitude+positions[i+1].latitude)/2, (positions[i].longitude+positions[i+1].longitude)/2)
+            }
+        }
+        return positions[positions.size/2]
+    }
+
+    private fun distanceBetweenPoints(p1: LatLng, p2: LatLng): Float {
+        return calculateDistanceDistance(p1.latitude, p1.longitude, p2.latitude, p2.longitude).toFloat()
+    }
+    private fun calculateDistanceDistance(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
+        return if (lat1 == lat2 && lon1 == lon2) {
+            0.0
+        } else {
+            val theta = lon1 - lon2
+            var dist = sin(Math.toRadians(lat1)) * sin(Math.toRadians(lat2)) + cos(Math.toRadians(lat1)) * cos(Math.toRadians(lat2)) * cos(Math.toRadians(theta))
+            dist = acos(dist)
+            dist = Math.toDegrees(dist)
+            dist *= 60 * 1.1515
+            dist *= 1.609344
+            dist * 1000
+        }
     }
 }
